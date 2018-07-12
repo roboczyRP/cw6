3c1efc588c3868a79afbe7e323991c8f42dee01c
roboczyRP <fofondoti@gmail.com>
2018-07-12 14:50:41 +0200

    new exercise

---
+++ application/Imagine/Gd/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rp
+ * Date: 12.07.18
+ * Time: 14:29
+ */
+
+namespace Imagine\Gd;
+
+
+class Test
+{
+
+}
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rp
+ * Date: 12.07.18
+ * Time: 14:23
+ */
+
+require('application/Imagine/Gd/Imagine.php');
+
+use Imagine\Gd\Imagine;
+
+$object = new Imagine();
